bc695f1cefe4a1e1ea33efdd38bf87f1a8989f7e
Merch Visoiu <merch@merchv.com>
2020-04-26 15:50:05 -0400

    Initial Commit

---
+++ BitriseTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BitriseTest
+//
+//  Created by Merch on 2020-04-26.
+//  Copyright © 2020 MerchV. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
